88c3d544919f858c33f8edc0a9dd8f5a4631a01d
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-08-19 01:10:04 +0530

    Fix: justify left the tab headings

--- app/components/TabButton.jsx
+++ app/components/TabButton.jsx
@@ -10,14 +10,17 @@ const TabButton = ({ active, selectTab, children }) => {
   const buttonClasses = active ? "text-white" : "text-[#ADB7BE]";
 
   return (
-    <button onClick={selectTab}>
-      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+    <button
+      onClick={selectTab}
+      className="flex flex-col items-start text-left mx-2"
+    >
+      <p className={`font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
       <motion.div
         animate={active ? "active" : "default"}
         variants={variants}
-        className="h-1 bg-amber-500 mt-2 mr-3"
+        className="h-1 bg-amber-500 mt-1"
       ></motion.div>
     </button>
   );

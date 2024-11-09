309377758be5dc6e0037e959b3cee4bdeefab89c
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-11-09 10:30:47 +0530

    fix: i don't know

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState } from "react";
-import GithubIcon from "../../../public/github-icon.svg";
-import LinkedinIcon from "../../../public/linkedin-icon.svg";
-import LeetCodeIcon from "../../../public/leetcode-icon.svg";
+import GithubIcon from "../../public/github-icon.svg";
+import LinkedinIcon from "../../public/linkedin-icon.svg";
+import LeetCodeIcon from "../../public/leetcode-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 

7c75b18d21d3becc09fc59329aa91ecb5a23eec9
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-12-01 17:09:20 +0530

    fix: remove indexOf due to causing errors

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -61,7 +61,7 @@ const Profile = () => {
               className="mb-12 px-1 inline-block py-1 w-fit sm:w-fit rounded-full bg-gradient-to-br from-amber-700 to-red-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Resume
+                Download Resume
               </span>
             </button>
           </div>
